c5c2bb872a7c738d722b27998c122bf3fc5fb5fd
henrique.claro <henrique.claro@conductor.com.br>
2018-09-27 13:29:23 -0300

    arguments fix

--- features/step_definitions/search_exam.rb
+++ features/step_definitions/search_exam.rb
@@ -4,21 +4,21 @@ Dado(/^que eu acesso a pagina principal$/) do
   @home.load
 end
 
-Quando(/^eu acesso a pagina de busca de exames$/) do |user|
+Quando(/^eu acesso a pagina de busca de exames$/) do 
   @home.access_list_clients
 end
 
-Quando(/^mostrar a tela de busca de exames e buscar o exame "([^"]*)"$/) do
+Quando(/^mostrar a tela de busca de exames e buscar o exame "([^"]*)"$/) do |exam|
   @exam = ExamPage.new
   @exam.exam_page_confirmation
   @exam.search_exam($default_data['exam_name'][exam])
 
 end
 
-Entao(/^consigo encontrar o exame "([^"]*)" na lista$/) do
+Entao(/^consigo encontrar o exame "([^"]*)" na lista$/) do |exam|
   @exam.confirm_valid_exam($default_data['exam_name'][exam])
 end
 
-Entao(/^nao consigo encontrar o exame "([^"]*)" na lista$/) do
+Entao(/^nao consigo encontrar o exame "([^"]*)" na lista$/) do |exam|
   @exam.confirm_valid_exam($default_data['exam_name'][exam])
 end
